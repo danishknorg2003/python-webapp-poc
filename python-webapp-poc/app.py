0620087c4e09fa63ce50bc94d0d82bbc82308e27
Danish Khan <danishkn2003@outlook.com>
2026-06-30 08:48:33 +0530

    Add project files.

---
+++ python-webapp-poc/app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <h1>Azure DevOps Python POC</h1>
+    <h2>Deployment Successful</h2>
+    """
+
+@app.route("/health")
+def health():
+    return {
+        "status":"healthy"
+    }
+
+if __name__=="__main__":
+    app.run(
+        host="0.0.0.0",
+        port=8000
+    )
